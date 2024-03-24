cf37d6d6b9bf7504408c6b20e43ee7c8b632ce87
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-03-24 23:46:25 +0300

    add elements

--- Whitch_Number/ViewController.swift
+++ Whitch_Number/ViewController.swift
@@ -19,7 +19,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     let checkButton: UIButton = {
         let button = UIButton()
+        button.addTarget(ViewController.self, action: #selector(checkNumbers), for: .touchUpInside)
         button.setTitle("Check", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .yellow
         button.frame = CGRect(x: 0, y: 0, width: 70, height: 100)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -27,12 +30,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     let resultLabel: UILabel = {
         let label = UILabel()
+        label.text = "Text"
+        label.font = UIFont(name: "Text", size: 20)
+        label.textColor = UIColor.black
+        label.frame = CGRect(x: 0, y: 0, width: 60, height: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkButton.addTarget(self, action: #selector(checkNumbers), for: .touchUpInside)
+        
+        setting()
+        
        
     }
 
@@ -42,9 +52,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     func setting(){
+        
         view.addSubview(insertNumber)
+        insertNumber.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        insertNumber.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        insertNumber.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        insertNumber.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
         view.addSubview(checkButton)
+        checkButton.topAnchor.constraint(equalTo: insertNumber.bottomAnchor, constant: 20).isActive = true
+        checkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
         view.addSubview(resultLabel)
+        resultLabel.topAnchor.constraint(equalTo: checkButton.bottomAnchor, constant: 20).isActive = true
+        resultLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
     }
 

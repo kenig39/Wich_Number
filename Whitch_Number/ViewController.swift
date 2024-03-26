7a3f78cf5586654196917b5f1f7e133c94983770
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-03-26 22:12:10 +0300

    finsh app

--- Whitch_Number/ViewController.swift
+++ Whitch_Number/ViewController.swift
@@ -24,11 +24,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     let checkButton: UIButton = {
         let button = UIButton()
-        button.addTarget(ViewController.self, action: #selector(checkNumbers), for: .touchUpInside)
         button.setTitle("Check", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.backgroundColor = .yellow
-        button.frame = CGRect(x: 0, y: 0, width: 70, height: 100)
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 2
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -41,9 +41,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    let resultNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Text", size: 20)
+        label.text = "Результат: "
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let roundLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Text", size: 20)
+        label.textColor = UIColor.black
+        label.frame = CGRect(x: 0, y: 0, width: 60, height: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        checkButton.addTarget(self, action: #selector(checkNumbers), for: .touchUpInside)
         
         setting()
         
@@ -51,19 +72,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @objc func checkNumbers() {
-        if self.round == 0 {
-            self.number = Int.random(in: 1...50)
-            self.resultLabel.text = String(self.number)
+        if  round == 0 {
+             number = Int.random(in: 1...50)
+            self.resultLabel.text = String(number)
             self.round = 1
         }
         else {
-            let numberTextField = Int(self.insertNumber.text ?? "")
-            if numberTextField > self.number {
-                
+            guard let numberTextField = Int(self.insertNumber.text ?? "") else {
+                return
             }
+            if numberTextField > self.number {
+                self.points += 50 - numberTextField + self.number
+            } else if
+                numberTextField < self.number {
+                self.points += 50 - self.number + numberTextField
+            }
+            else {
+                self.points += 50
+            }
+            if self.round == 5 {
+                
+                let alert = UIAlertController(title: "Игра Окончена",
+                                              message: "Вы набрани \(self.points) очков ",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Начать заного",
+                                              style: .default))
+                self.present(alert, animated: true)
+                self.round = 1
+                self.points = 0
+            }
+            else {
+                self.round += 1
+            }
+            self.number = Int.random(in: 1...50)
             
+            self.resultLabel.text = String(self.number)
+            
+            self.roundLabel.text = String(self.round)
         }
-        
     }
     
     
@@ -78,11 +124,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         view.addSubview(checkButton)
         checkButton.topAnchor.constraint(equalTo: insertNumber.bottomAnchor, constant: 20).isActive = true
+        checkButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        checkButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
         checkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         view.addSubview(resultLabel)
         resultLabel.topAnchor.constraint(equalTo: checkButton.bottomAnchor, constant: 20).isActive = true
         resultLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        view.addSubview(resultNameLabel)
+        resultNameLabel.topAnchor.constraint(equalTo: checkButton.bottomAnchor, constant: 20).isActive = true
+        resultNameLabel.rightAnchor.constraint(equalTo: view.leftAnchor, constant: 90 ).isActive = true
+        
+        
+        view.addSubview(roundLabel)
+        roundLabel.topAnchor.constraint(equalTo: resultLabel.bottomAnchor, constant: 20).isActive = true
+        roundLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
     }
 
